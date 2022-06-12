e260bd06b3b6109ac08694a26a804b649244c592
Denty <dentymods.axuka@gmail.com>
2022-06-11 22:38:28 -0400

    .

--- ShowReportsInGame/Config.cs
+++ ShowReportsInGame/Config.cs
@@ -13,11 +13,14 @@ namespace ShowReportsInGame
 		public bool EnableDebug { get; set; } = false;
 
 		//Normal self report stuffz
+		[Description("Should a broadcast be sent to a player when they try reporting themself in a normal report?")]
+		public bool NormalSelfReportBroadcastEnabled { get; set; } = true;
+
 		[Description("Broadcast message sent to player reporting themself in a normal report")]
-		public string NormalSelfReportMessage { get; set; } = "Why are you trying to report yourself?";
+		public string NormalSelfReportBroadcast { get; set; } = "Why are you trying to report yourself?";
 
 		[Description("Broadcast message duration sent to player reporting themself in a normal report")]
-		public ushort NormalSelfReportMessageDuration { get; set; } = 5;
+		public ushort NormalSelfReportBroadcastDuration { get; set; } = 5;
 
 		[Description("Duration for hint message sent to all RA access players about self report in a normal report")]
 		public ushort NormalSelfReportHintDuration { get; set; } = 5;
@@ -28,11 +31,14 @@ namespace ShowReportsInGame
 
 
 		//Cheater self report stuffz
+		[Description("Should a broadcast be sent to a player when they try reporting themself in a cheater report?")]
+		public bool CheaterSelfReportBroadcastEnabled { get; set; } = true;
+
 		[Description("Broadcast message sent to player reporting themself in a normal report")]
-		public string CheaterSelfReportMessage { get; set; } = "Why are you trying to report yourself?";
+		public string CheaterSelfReportBroadcast { get; set; } = "Why are you trying to report yourself?";
 
 		[Description("Broadcast message duration sent to player reporting themself in a normal report")]
-		public ushort CheaterSelfReportMessageDuration { get; set; } = 5;
+		public ushort CheaterSelfReportBroadcastDuration { get; set; } = 5;
 
 		[Description("Duration for hint message sent to all RA access players about self report in a normal report")]
 		public ushort CheaterSelfReportHintDuration { get; set; } = 5;
@@ -40,5 +46,47 @@ namespace ShowReportsInGame
 		//Cheater report stuffz
 		[Description("Duration for hint message sent to all RA access players about normal report")]
 		public ushort CheaterReportHintDuration { get; set; } = 5;
+
+
+		//Custom hint strings
+		//Normal selfreport custom text
+		[Description("Normal selfreport hint first line")]
+		public string NormalSelfreportStringOne { get; set; } = @"<size=20><color=red>[</color><color=yellow>ShowReportsInGame</color><color=red>]</color></size>";
+
+		[Description("Normal selfreport hint second line")]
+		public string NormalSelfreportStringTwo { get; set; } = @"<size=20><color=red>/|\</color>[<color=red>NOTE: THIS IS A SELF-REPORT</color>]<color=red>/|\</color></size>";
+
+		[Description("Normal selfreport hint third line")]
+		public string NormalSelfreportStringThree { get; set; } = @"<size=20>[<color=yellow>Player Report, Check Console (`) For Info</color>]</size>";
+
+		//Normal report custom text
+		[Description("Normal report hint first line")]
+		public string NormalreportStringOne { get; set; } = @"<size=20><color=red>[</color><color=yellow>ShowReportsInGame</color><color=red>]</color></size>";
+
+		[Description("Normal report hint second line")]
+		public string NormalreportStringTwo { get; set; } = @"<size=20><color=red>/|\</color>[<color=green>NOTE: This is a normal report.</color>]<color=red>/|\</color></size>";
+
+		[Description("Normal report hint third line")]
+		public string NormalreportStringThree { get; set; } = @"<size=20>[<color=yellow>Player Report, Check Console (`) For Info</color>]</size>";
+
+		//Cheater selfreport custom text
+		[Description("Cheater selfreport hint first line")]
+		public string CheaterSelfreportStringOne { get; set; } = @"<size=20><color=red>[</color><color=yellow>ShowReportsInGame</color><color=red>]</color></size>";
+
+		[Description("Cheater selfreport hint second line")]
+		public string CheaterSelfreportStringTwo { get; set; } = @"<size=20><color=red>/|\</color>[<color=red>WARNING: THIS IS A CHEATER SELFREPORT</color>]<color=red>/|\</color></size>";
+
+		[Description("Cheater selfreport hint third line")]
+		public string CheaterSelfreportStringThree { get; set; } = @"<size=20>[<color=yellow>Cheater Report, Check Console (`) For Info</color>]</size>";
+
+		//Cheater report custom text
+		[Description("Cheater report hint first line")]
+		public string CheaterreportStringOne { get; set; } = @"<size=20><color=red>[</color><color=yellow>ShowReportsInGame</color><color=red>]</color></size>";
+
+		[Description("Cheater report hint second line")]
+		public string CheaterreportStringTwo { get; set; } = @"<size=20><color=red>/|\</color>[<color=red>WARNING: THIS IS A CHEATER REPORT</color>]<color=red>/|\</color></size>";
+
+		[Description("Cheater report hint third line")]
+		public string CheaterreportStringThree { get; set; } = @"<size=20>[<color=yellow>Cheater Report, Check Console (`) For Info</color>]</size>";
 	}
 }
--- ShowReportsInGame/EventHandler.cs
+++ ShowReportsInGame/EventHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Exiled.Events.EventArgs;
+using Exiled.API.Features;
 using Player = Exiled.API.Features.Player;
 
 namespace ShowReportsInGame
@@ -8,12 +9,25 @@ namespace ShowReportsInGame
     {
         public void LocalReport(LocalReportingEventArgs ev)
         {
+            string SelfreportHintOne = NormalSelfreportStringOne(ev);
+            string SelfreportHintTwo = NormalSelfreportStringTwo(ev);
+            string SelfreportHintThree = NormalSelfreportStringThree(ev);
+
+            string ReportHintOne = NormalreportStringOne(ev);
+            string ReportHintTwo = NormalreportStringTwo(ev);
+            string ReportHintThree = NormalreportStringThree(ev);
+
+
             var ReportTimeStamp = DateTime.Now.ToString("HH:mm:sstt MM/dd/yyyy");
 
             //If someone reports themself
             if (ev.Target.UserId == ev.Issuer.UserId)
             {
-                ev.Issuer.Broadcast(ShowReportsInGame.Singleton.Config.NormalSelfReportMessageDuration, ShowReportsInGame.Singleton.Config.NormalSelfReportMessage);
+                if (ShowReportsInGame.Singleton.Config.NormalSelfReportBroadcastEnabled == true)
+                {
+                    ev.Issuer.Broadcast(ShowReportsInGame.Singleton.Config.NormalSelfReportBroadcastDuration, ShowReportsInGame.Singleton.Config.NormalSelfReportBroadcast);
+                    Log.Debug($"Sending normal selfreport broadcast to {ev.Target}", ShowReportsInGame.Singleton.Config.EnableDebug);
+                }
 
                 //Sends hint and console message to all players with RA access about selfreport moment
                 foreach (var StaffMember in Player.List)
@@ -35,11 +49,11 @@ namespace ShowReportsInGame
                             Environment.NewLine,
                             Environment.NewLine,
                             Environment.NewLine,
-                            @"<size=20><color=red>/|\</color>[<color=red>NOTE: THIS IS A SELF-REPORT</color>]<color=red>/|\</color></size>",
+                            SelfreportHintOne,
                             Environment.NewLine,
-                            "<size=20>[<color=yellow>Player Report, Check Console (`) For Info</color>]</size>",
+                            SelfreportHintTwo,
                             Environment.NewLine,
-                            "<size=20><color=orange>Timestamp (24H)</color>: <color=red>", ReportTimeStamp, "</color></size>"), duration: ShowReportsInGame.Singleton.Config.NormalSelfReportHintDuration);
+                            SelfreportHintThree), duration: ShowReportsInGame.Singleton.Config.NormalSelfReportHintDuration);
 
                         //Sends console message about report
                         StaffMember.SendConsoleMessage(
@@ -80,11 +94,11 @@ namespace ShowReportsInGame
                             Environment.NewLine,
                             Environment.NewLine,
                             Environment.NewLine,
-                            @"<size=20><color=red>/|\</color>[<color=green>NOTE: This is a normal report.</color>]<color=red>/|\</color></size>",
+                            ReportHintOne,
                             Environment.NewLine,
-                            "<size=20>[<color=yellow>Player Report, Check Console (`) For Info</color>]</size>",
+                            ReportHintTwo,
                             Environment.NewLine,
-                            "<size=20><color=orange>Timestamp (24H)</color>: <color=red>", ReportTimeStamp, "</color></size>"), duration: ShowReportsInGame.Singleton.Config.NormalReportHintDuration);
+                            ReportHintThree), duration: ShowReportsInGame.Singleton.Config.NormalReportHintDuration);
 
                         //Sends console message about report
                         StaffMember.SendConsoleMessage(
@@ -105,13 +119,25 @@ namespace ShowReportsInGame
 
         public void CheaterReport(ReportingCheaterEventArgs ev)
         {
+            string SelfreportHintOne = CheaterSelfreportStringOne(ev);
+            string SelfreportHintTwo = CheaterSelfreportStringTwo(ev);
+            string SelfreportHintThree = CheaterSelfreportStringThree(ev);
+
+            string ReportHintOne = CheaterreportStringOne(ev);
+            string ReportHintTwo = CheaterreportStringTwo(ev);
+            string ReportHintThree = CheaterreportStringThree(ev);
+
+
             var ReportTimeStamp = DateTime.Now.ToString("HH:mm:sstt MM/dd/yyyy");
 
             //If someone reports themself
             if (ev.Target.UserId == ev.Issuer.UserId)
             {
-                //If someone reports themself
-                ev.Issuer.Broadcast(ShowReportsInGame.Singleton.Config.CheaterSelfReportMessageDuration, ShowReportsInGame.Singleton.Config.CheaterSelfReportMessage);
+                if (ShowReportsInGame.Singleton.Config.CheaterSelfReportBroadcastEnabled == true)
+                {
+                    ev.Issuer.Broadcast(ShowReportsInGame.Singleton.Config.CheaterSelfReportBroadcastDuration, ShowReportsInGame.Singleton.Config.CheaterSelfReportBroadcast);
+                    Log.Debug($"Sending normal selfreport broadcast to {ev.Target}", ShowReportsInGame.Singleton.Config.EnableDebug);
+                }
 
                 //Sends hint and console message to all players with RA access about cheater selfreport moment
                 foreach (var StaffMember in Player.List)
@@ -133,13 +159,11 @@ namespace ShowReportsInGame
                             Environment.NewLine,
                             Environment.NewLine,
                             Environment.NewLine,
-                            @"<size=20><color=red>/|\</color>[<color=red>WARNING: THIS IS A CHEATER REPORT</color>]<color=red>/|\</color></size>",
+                            SelfreportHintOne,
                             Environment.NewLine,
-                            @"<size=20><color=red>/|\</color>[<color=red>NOTE: THIS IS A SELF-REPORT</color>]<color=red>/|\</color></size>",
+                            SelfreportHintTwo,
                             Environment.NewLine,
-                            "<size=20>[<color=yellow>Cheater Report, Check Console (`) For Info</color>]</size>",
-                            Environment.NewLine,
-                            "<size=20><color=orange>Timestamp (24H)</color>: <color=red>", ReportTimeStamp, "</color></size>"), duration: ShowReportsInGame.Singleton.Config.CheaterSelfReportHintDuration);
+                            SelfreportHintThree), duration: ShowReportsInGame.Singleton.Config.CheaterSelfReportHintDuration);
 
                         //Sends console message about report
                         StaffMember.SendConsoleMessage(
@@ -181,11 +205,11 @@ namespace ShowReportsInGame
                             Environment.NewLine,
                             Environment.NewLine,
                             Environment.NewLine,
-                            @"<size=20><color=red>/|\</color>[<color=red>WARNING: THIS IS A CHEATER REPORT</color>]<color=red>/|\</color></size>",
+                            ReportHintOne,
                             Environment.NewLine,
-                            "<size=20>[<color=yellow>Cheater Report, Check Console (`) For Info</color>]</size>",
+                            ReportHintTwo,
                             Environment.NewLine,
-                            "<size=20><color=orange>Timestamp (24H)</color>: <color=red>", ReportTimeStamp, "</color></size>"), duration: ShowReportsInGame.Singleton.Config.CheaterReportHintDuration);
+                            ReportHintThree), duration: ShowReportsInGame.Singleton.Config.CheaterReportHintDuration);
 
                         //Sends console message about report
                         StaffMember.SendConsoleMessage(
@@ -203,6 +227,60 @@ namespace ShowReportsInGame
                     }
                 }
             }
+        }
+
+
+        //Code orginally from CommonUtils plugin since its really helpful method for me
+        private string NormalSelfreportStringOne(LocalReportingEventArgs LocalReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.NormalSelfreportStringOne.Replace("%IssuerUserId%", LocalReporting.Issuer.UserId).Replace("%IssuerGameId%", LocalReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", LocalReporting.Issuer.Nickname).Replace("%IssuerRole%", LocalReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", LocalReporting.Target.UserId).Replace("%TargetGameId%", LocalReporting.Target.Id.ToString()).Replace("%TargetNickname%", LocalReporting.Target.Nickname).Replace("%TargetRole%", LocalReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", LocalReporting.Reason);
+        }
+        private string NormalSelfreportStringTwo(LocalReportingEventArgs LocalReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.NormalSelfreportStringTwo.Replace("%IssuerUserId%", LocalReporting.Issuer.UserId).Replace("%IssuerGameId%", LocalReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", LocalReporting.Issuer.Nickname).Replace("%IssuerRole%", LocalReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", LocalReporting.Target.UserId).Replace("%TargetGameId%", LocalReporting.Target.Id.ToString()).Replace("%TargetNickname%", LocalReporting.Target.Nickname).Replace("%TargetRole%", LocalReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", LocalReporting.Reason);
+        }
+        private string NormalSelfreportStringThree(LocalReportingEventArgs LocalReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.NormalSelfreportStringThree.Replace("%IssuerUserId%", LocalReporting.Issuer.UserId).Replace("%IssuerGameId%", LocalReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", LocalReporting.Issuer.Nickname).Replace("%IssuerRole%", LocalReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", LocalReporting.Target.UserId).Replace("%TargetGameId%", LocalReporting.Target.Id.ToString()).Replace("%TargetNickname%", LocalReporting.Target.Nickname).Replace("%TargetRole%", LocalReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", LocalReporting.Reason);
+        }
+
+        private string NormalreportStringOne(LocalReportingEventArgs LocalReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.NormalreportStringOne.Replace("%IssuerUserId%", LocalReporting.Issuer.UserId).Replace("%IssuerGameId%", LocalReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", LocalReporting.Issuer.Nickname).Replace("%IssuerRole%", LocalReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", LocalReporting.Target.UserId).Replace("%TargetGameId%", LocalReporting.Target.Id.ToString()).Replace("%TargetNickname%", LocalReporting.Target.Nickname).Replace("%TargetRole%", LocalReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", LocalReporting.Reason);
+        }
+        private string NormalreportStringTwo(LocalReportingEventArgs LocalReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.NormalreportStringTwo.Replace("%IssuerUserId%", LocalReporting.Issuer.UserId).Replace("%IssuerGameId%", LocalReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", LocalReporting.Issuer.Nickname).Replace("%IssuerRole%", LocalReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", LocalReporting.Target.UserId).Replace("%TargetGameId%", LocalReporting.Target.Id.ToString()).Replace("%TargetNickname%", LocalReporting.Target.Nickname).Replace("%TargetRole%", LocalReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", LocalReporting.Reason);
+        }
+        private string NormalreportStringThree(LocalReportingEventArgs LocalReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.NormalreportStringThree.Replace("%IssuerUserId%", LocalReporting.Issuer.UserId).Replace("%IssuerGameId%", LocalReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", LocalReporting.Issuer.Nickname).Replace("%IssuerRole%", LocalReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", LocalReporting.Target.UserId).Replace("%TargetGameId%", LocalReporting.Target.Id.ToString()).Replace("%TargetNickname%", LocalReporting.Target.Nickname).Replace("%TargetRole%", LocalReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", LocalReporting.Reason);
+        }
+
+        private string CheaterSelfreportStringOne(ReportingCheaterEventArgs CheaterReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.CheaterSelfreportStringOne.Replace("%IssuerUserId%", CheaterReporting.Issuer.UserId).Replace("%IssuerGameId%", CheaterReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", CheaterReporting.Issuer.Nickname).Replace("%IssuerRole%", CheaterReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", CheaterReporting.Target.UserId).Replace("%TargetGameId%", CheaterReporting.Target.Id.ToString()).Replace("%TargetNickname%", CheaterReporting.Target.Nickname).Replace("%TargetRole%", CheaterReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", CheaterReporting.Reason);
+        }
+        private string CheaterSelfreportStringTwo(ReportingCheaterEventArgs CheaterReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.CheaterSelfreportStringTwo.Replace("%IssuerUserId%", CheaterReporting.Issuer.UserId).Replace("%IssuerGameId%", CheaterReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", CheaterReporting.Issuer.Nickname).Replace("%IssuerRole%", CheaterReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", CheaterReporting.Target.UserId).Replace("%TargetGameId%", CheaterReporting.Target.Id.ToString()).Replace("%TargetNickname%", CheaterReporting.Target.Nickname).Replace("%TargetRole%", CheaterReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", CheaterReporting.Reason);
+        }
+        private string CheaterSelfreportStringThree(ReportingCheaterEventArgs CheaterReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.CheaterSelfreportStringThree.Replace("%IssuerUserId%", CheaterReporting.Issuer.UserId).Replace("%IssuerGameId%", CheaterReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", CheaterReporting.Issuer.Nickname).Replace("%IssuerRole%", CheaterReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", CheaterReporting.Target.UserId).Replace("%TargetGameId%", CheaterReporting.Target.Id.ToString()).Replace("%TargetNickname%", CheaterReporting.Target.Nickname).Replace("%TargetRole%", CheaterReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", CheaterReporting.Reason);
+        }
+
+        private string CheaterreportStringOne(ReportingCheaterEventArgs CheaterReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.CheaterreportStringOne.Replace("%IssuerUserId%", CheaterReporting.Issuer.UserId).Replace("%IssuerGameId%", CheaterReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", CheaterReporting.Issuer.Nickname).Replace("%IssuerRole%", CheaterReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", CheaterReporting.Target.UserId).Replace("%TargetGameId%", CheaterReporting.Target.Id.ToString()).Replace("%TargetNickname%", CheaterReporting.Target.Nickname).Replace("%TargetRole%", CheaterReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", CheaterReporting.Reason);
+        }
+        private string CheaterreportStringTwo(ReportingCheaterEventArgs CheaterReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.CheaterreportStringTwo.Replace("%IssuerUserId%", CheaterReporting.Issuer.UserId).Replace("%IssuerGameId%", CheaterReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", CheaterReporting.Issuer.Nickname).Replace("%IssuerRole%", CheaterReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", CheaterReporting.Target.UserId).Replace("%TargetGameId%", CheaterReporting.Target.Id.ToString()).Replace("%TargetNickname%", CheaterReporting.Target.Nickname).Replace("%TargetRole%", CheaterReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", CheaterReporting.Reason);
+        }
+        private string CheaterreportStringThree(ReportingCheaterEventArgs CheaterReporting)
+        {
+            return ShowReportsInGame.Singleton.Config.CheaterreportStringThree.Replace("%IssuerUserId%", CheaterReporting.Issuer.UserId).Replace("%IssuerGameId%", CheaterReporting.Issuer.Id.ToString()).Replace("%IssuerNickname%", CheaterReporting.Issuer.Nickname).Replace("%IssuerRole%", CheaterReporting.Issuer.Role.Type.ToString()).Replace("%TargetUserId%", CheaterReporting.Target.UserId).Replace("%TargetGameId%", CheaterReporting.Target.Id.ToString()).Replace("%TargetNickname%", CheaterReporting.Target.Nickname).Replace("%TargetRole%", CheaterReporting.Target.Role.Type.ToString()).Replace("%ReportReason%", CheaterReporting.Reason);
         }
     }
 }
