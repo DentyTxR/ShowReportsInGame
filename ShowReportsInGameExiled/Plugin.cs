2f625bdf79514d75c468547a6bc21f56bd27bcfa
Denty <dentymods.axuka@gmail.com>
2025-05-04 13:52:46 -0400

    updated exiled version, will make labapi version... maybe

--- ShowReportsInGameExiled/Plugin.cs
+++ ShowReportsInGameExiled/Plugin.cs
@@ -4,19 +4,16 @@ using ServerEvent = Exiled.Events.Handlers.Server;
 
 namespace ShowReportsInGameExiled
 {
-
     public class ShowReportsInGame : Plugin<Config>
     {
-
         private EventHandler EventHandler;
         public static ShowReportsInGame Singleton;
 
         public override string Name { get; } = "ShowReportsInGame";
         public override string Author { get; } = "Denty";
         public override string Prefix { get; } = "ShowReportsInGame";
-        public override Version Version { get; } = new Version(2, 2, 6);
+        public override Version Version { get; } = new Version(2, 2, 7);
         public override Version RequiredExiledVersion { get; } = new Version(9, 3, 0);
-
 
         public override void OnEnabled()
         {
@@ -29,13 +26,10 @@ namespace ShowReportsInGameExiled
             base.OnEnabled();
         }
 
-
         public override void OnDisabled()
         {
-
             ServerEvent.LocalReporting -= EventHandler.LocalReport;
             ServerEvent.ReportingCheater -= EventHandler.CheaterReport;
-
 
             EventHandler = null;
             base.OnDisabled();
